1de770363a0034b2a5c472ec31a7db790af8746a
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-05-06 00:37:29 +0200

    added css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 
 class App extends React.Component {
@@ -8,21 +9,26 @@ class App extends React.Component {
 
     componentDidMount()
     {
-    axios.get();
+    this.fetchAdvice();
     }
     fetchAdvice =() => {
         axios.get('https://api.adviceslip.com/advice')
         .then((response) => {
-            console.log(response);
+            const {advice} = response.data.slip;
+             this.setState({advice});
           })
         .catch((error) => {
         console.log(error);
          });
 }
     render()
-    {
+    { const {advice}=this.state;
         return ( 
-        <h1>APP</h1>
+        <div className="app">
+            <div className="card">
+                <h1 className="heading">{advice}</h1>
+            </div>
+            </div>
         );
     }
 }
